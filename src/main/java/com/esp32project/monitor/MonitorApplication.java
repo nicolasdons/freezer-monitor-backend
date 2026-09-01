2e7faea9632400200a6e925621dd2ae87e69cdb0
Nicolas <nicolasnssoares@gmail.com>
2026-09-01 11:36:36 -0300

    feat: estrutura inicial do projeto spring boot

---
+++ src/main/java/com/esp32project/monitor/MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.esp32project.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+}
